8b3e6fa5c23bb6a09e39e42074a76ed8bf60bbee
Sachin-Israni <sachin.israni@saeculumsolutions.com>
2023-06-15 18:55:35 +0530

    15 June

---
+++ June/15 June/case.js
@@ -0,0 +1,54 @@
+// "use strict";
+// a="set";
+// console.log(a);
+
+
+
+// let a;
+// a=10;
+// console.log(a)
+
+
+// function absd(){
+//     var abc;
+//     console.log(abc)
+// }
+
+// absd()
+
+
+
+// -----------------------  keywords as variablename  ---------------------- 
+
+// var let=10;
+// console.log(let)
+// var const=100;   // Const is not used as vname in var
+// var var=26;  // Var is also not used as vname in var
+
+// let var=102;
+// let let=2982;        // All three are not used as variable name in let
+// let const=2982;
+
+// const var=29;
+// const let=20;   // All three are not used as variable name in const
+// const const=20;
+
+// ------------------------ Hoisting ---------------
+
+// a=10;
+// {
+//     console.log(a)                  // Var Variable 
+//     var a;
+// }
+
+// s=20;
+// {
+//     console.log(s)                  // Let Variable  
+//     let s;
+// }
+
+// ab=20;
+// {
+//     console.log(ab)                  // Const Variable  
+//     const ab;
+// }    
