bce9e5f84d49a2537e9c9bead3fa90ea200a6c81
Sachin-Israni <sachin.israni@saeculumsolutions.com>
2023-06-16 19:00:59 +0530

    16 June

---
+++ June/16 June/Data.js
@@ -0,0 +1,77 @@
+// ---------------- School data
+
+
+// let Airpot = {
+//     Std: [
+
+//         {
+//             "STd1-5": 
+//                 {
+//                     "Number of Teachers": 25,
+//                     "Number Of Students": 300,
+//                     "Number Of Classes": 15
+//                 }
+
+//         },
+
+//         {
+//             "STd5-10": [
+
+//                     "Number of Teachers: 38",
+//                     "Number Of Students: 410",
+//                     "Number Of Classes: 20"
+
+//             ]
+//         },
+
+//         {
+//             "STd11-12": 
+//                 {
+//                     "Number of Teachers": 16,
+//                     "Number Of Students": 150,
+//                     "Number Of Classes": 6
+//                 }
+
+//         }
+//     ]
+// }
+
+// Std = Std.sort();
+// console.log(Airpot.Std[1]);
+// console.log(Airpot.Std.length);
+
+
+
+// let names = [20,10,23,'esde',undefined];
+// console.log(names.lenght);
+// names = names.sort();
+// names.push("This is me")
+// console.log(names);
+
+// let myname = new Array(10);
+// myname.push("This is me");
+// console.log(myname);;
+
+// let obj1 = {
+//     age: 20
+// }
+// let obj2 = obj1;
+// obj2.age = 38;
+// console.log(obj1) 
+
+
+
+// let array=[10.20, 102 , ['name',10,20,20,['Hurry you win 1000rs press 1',[100000]]]]
+// console.log(array[2][4][1]);
+
+
+// const course={
+//     lessons:10,
+//     lecture:3,
+//     title:'JavaScript',
+//     lesson() {
+//         console.log('Hello Guys')
+//     }
+// }
+// console.log(course.lessons);
+// course.lesson();
---
+++ June/16 June/Operaters.js
@@ -0,0 +1,43 @@
+
+
+// let sname = 10;
+// {
+//     console.log(sname)
+//     let obj1={
+
+//         sname :'sachin',
+//     }
+//     console.log(sname)
+// }
+// sname = 329;
+// console.log(sname)
+
+// ----------------------- OPERATERS -----------------------------
+
+
+// --------------------- Arithematic -----------------------------
+
+a=20;
+b=10;
+console.log("a+b =" , a+b);
+console.log("a-b =" , a-b);
+console.log("a/b =" , a/b);
+console.log("a*b =" , a*b);
+console.log("a**b =" , a**b);
+console.log("a%b =" , a%b);
+
+
+console.log("Addition");
+console.log("a++ =" , a++);
+console.log("a =" , a);
+console.log("++a =" , ++a);
+console.log("a =" , a);
+console.log("Substraction");
+console.log("a-- =" , a--);
+console.log("a =" , a);
+console.log("--a =" , --a);
+console.log("a =" , a);
+
+// ---------------------- Logical ------------------------------
+
+
