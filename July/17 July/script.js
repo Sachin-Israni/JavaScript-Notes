075ca18b91705355049f53b0331d17fbdefd7737
Sachin-Israni <sachin.israni@saeculumsolutions.com>
2023-07-17 12:45:08 +0530

    Work before 11 AM

---
+++ July/17 July/script.js
@@ -0,0 +1,36 @@
+// Open a document to write to it
+// document.open();
+
+// Write the content of the document
+// document.write("<p>The one and only content.</p>");
+
+// Close the document
+// document.close();
+
+
+// --------------------------------------------------------------
+
+// const node = document.getElementById("div1");
+// const a = document.createAttribute("my_attrib");
+// a.value = "newVal";
+// console.log(a); // "newVal"
+
+// --------------------------------------------------------------
+
+// let para = document.getElementById('para')
+
+// function run() {
+//     alert('Hello')
+// }
+// para.addEventListener('mouseover', run())
+// function toggleHide() {
+//     let btn = document.getElementById('btn')
+//     let para = document.getElementById('para')
+//     if (para.style.display != 'none') {
+//         para.style.display = 'none';
+//     }
+//     else {
+//         para.style.display = 'block';
+//     }
+// }
+
