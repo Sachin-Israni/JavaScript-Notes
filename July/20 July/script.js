271b9eb126bca4c09cf7f47747c347b50d44915a
Sachin-Israni <sachin.israni@saeculumsolutions.com>
2023-07-20 19:28:35 +0530

    DOM methods and event Listeners

--- July/20 July/script.js
+++ July/20 July/script.js
@@ -16,9 +16,11 @@ let para = document.getElementById('para')
 btn.addEventListener('click', () => {
     if (para.style.display != 'none') {
         para.style.display = 'none'
+        btn.innerHTML = 'Click Me Again'
     }
     else {
         para.style.display = 'block'
+        btn.innerHTML = 'Click Me'
     }
 })
 
@@ -99,7 +101,97 @@ dbl.addEventListener('contextmenu', () => {
 
 let resize = document.getElementById('resize')
 window.addEventListener('resize', () => {
-    resize.innerHTML=Math.random()
+    resize.innerHTML = '<b>Here I Am!</b>'
 })
 
+// --------------------------------onchange Event
 
+function myFunction() {
+    var x = document.getElementById("letdoit").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+}
+
+// function foo(ele) {
+//     console.log(ele.getAttribute());
+// }
+// --------------------------------Context Menu Event
+
+// let ids = document.getElementById('letdoit')
+// ids.addEventListener('change', () => {
+//     resize.innerHTML = 'You entered' + ids
+
+// })
+
+// --------------------------------Copy,Paste & Cut Event
+
+let ids = document.getElementById('letdoit')
+ids.addEventListener('copy', () => {
+    ids.style.backgroundColor = 'aqua'
+})
+ids.addEventListener('paste', () => {
+    ids.style.backgroundColor = 'lightgreen'
+})
+ids.addEventListener('cut', () => {
+    ids.style.backgroundColor = 'red'
+})
+
+// ------------------------------------ setTimeout
+function greet(name) {
+    console.log('Good Morning' + name);
+    console.log('Example of setTimeout');
+}
+setTimeout(greet, 3000, ' Sachin')
+
+
+//------------------------------------------ prevent default event
+
+let asd = document.getElementById('asd')
+let checking = document.getElementById('checking')
+checking.addEventListener('click', (e) => {
+    e.preventDefault()
+})
+function check() {
+    if (checking.hasAttribute('checked')) {
+        checking.removeAttribute('checked')
+        asd.innerHTML = 'NO'
+    }
+    else if (!checking.hasAttribute('checked')) {
+        checking.setAttribute('checked', '')
+        asd.innerHTML = 'Yes'
+    }
+    // checking.getAttribute('checked') ? checking.removeAttribute('checked') : checking.setAttribute('checked', '')
+    // asd.innerHTML = 'YES'
+}
+asd.addEventListener('click', () => {
+    // check() {
+    // if (checking.hasAttribute('checked')) {
+    //     checking.removeAttribute('checked')
+    //     asd.innerText = 'NO'
+    // }
+    // else {
+    //     checking.setAttribute('checked', '')
+    //     asd.innerText = 'Yes'
+    // }
+    // asd.innerHTML = 'YES'
+    checking.hasAttribute('checked') ? checking.removeAttribute('checked') : checking.setAttribute('checked', '')
+    // }
+})
+
+// console.log(checking.hasAttribute('checked'));
+console.log(document.getElementById('para').innerHTML)
+console.log(document.getElementById('para').innerText)
+console.log(document.getElementById('para').textContent)
+
+// let getId = document.querySelector('body')
+let getId = document.getElementsByTagName('body')[0].children
+// console.log(getId);
+for (let i = 0; i < getId.length; i++) {
+    getId[i].addEventListener('click', (e) => {
+        if (e.target.id == '') {
+            document.getElementById('getID').innerText = "No ID"
+        }
+        else {
+            document.getElementById('getID').innerText = e.target.id
+        }
+    })
+}
