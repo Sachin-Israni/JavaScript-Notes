25f937cd5c64c1a71ad1b70f91255fc530aac630
Sachin-Israni <sachin.israni@saeculumsolutions.com>
2023-08-04 19:03:13 +0530

    Image upload Functionality and Chat box

--- August/4 Aug/script.js
+++ August/4 Aug/script.js
@@ -1,41 +1,46 @@
 // ---------------------------
 
-// let arr = [
-//     { type: 'name', value: 20 },
-//     { type: 'other', value: 10 },
-//     { type: 'audio', value: 18 },
-//     { type: 'video', value: 22 },
-//     { type: 'name', value: 2 },
-//     { type: 'other', value: 25 },
-//     { type: 'audio', value: 30 }
-// ]
+let arr = [
+    { type: 'name', value: 20 },
+    { type: 'other', value: 10 },
+    { type: 'audio', value: 18 },
+    { type: 'video', value: 22 },
+    { type: 'name', value: 2 },
+    { type: 'other', value: 25 },
+    { type: 'audio', value: 30 }
+]
 
-// const { type, value } = arr[0]
-// console.log(type);
-// console.log(value);
+const { type, value } = arr[0]
+console.log(type);
+console.log(value);
 
-// let sumByType = {};
-//--- type value 
+// -------------------------- type value 
+
 // let sumByType = {sname:20};
 // let newA={type:'sname',noty:'other'}
 // const {type,noty}=newA
 // console.log(sumByType[type]);
 
-// arr.forEach(item => {
-//     const { type, value } = item;
-//     sumByType[type] = type +'   ' + (sumByType[type] || 0) + value;
-//     // console.log(sumByType[type] || 0);
-//     console.log(sumByType[type]);
-// });
+//----------------------------
+
+let sumByType = {};
+arr.forEach(item => {
+    const { type, value } = item;
+    // console.log(sumByType[type]);
+    sumByType[type] = (sumByType[type] || 0) + value;
+});
+    // console.log(sumByType[type] = (sumByType[type] || 0) + value);
+    // sumByType[type] = type +'   ' + (sumByType[type] || 0) + value;
+    // console.log(sumByType[type] || 0);
+console.log(sumByType);
+
+let output = Object.keys(sumByType).map(type => ({
+    type: type,
+    value: sumByType[type]
+}));
 
 
-// let output = Object.keys(sumByType).map(type => ({
-//   type,
-//   value: sumByType[type]
-// }));
-
-// console.log(output);
-// console.log(sumByType);
+console.log(output);
 
 
 
@@ -67,11 +72,17 @@
 
 
 
-let send = document.getElementById('send')
-let main = document.getElementsByClassName('pParent')[0]
+// let send = document.getElementById('send')
+// let main = document.getElementsByClassName('pParent')[0]
 
-let input = document.getElementById('input')
-send.addEventListener('click',()=> {
-    main.innerHTML+=`<div class='main2'><p>${input.value}</p><div>`
-    input.value=''
-})      
+// let input = document.getElementById('input')
+// send.addEventListener('click',()=> {
+//     main.innerHTML+=`<div class='main2'><p>${input.value}</p><div>`
+//     input.value=''
+// })      
+
+
+
+
+
+
